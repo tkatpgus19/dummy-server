6e69084cad01db4f2ae06d094f188c0a66b33f98
sehyun <germs1020@gmail.com>
2023-03-19 16:06:59 +0900

    추가 객체 생성 및 post url 테스트

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ const result = [
     {id: 3, textCategory:'front', textTitle:'title3', textBody:'bodybodybody3', createdDate:'date'},
     {id: 4, textCategory:'front', textTitle:'title4', textBody:'bodybodybody4', createdDate:'date'},
     {id: 5, textCategory:'front', textTitle:'title5', textBody:'bodybodybody5', createdDate:'date'},
+    {id: 6, textCategory:'front', textTitle:'title1', textBody:'bodybodybody1', createdDate:'date'},
+    {id: 7, textCategory:'front', textTitle:'title2', textBody:'bodybodybody2', createdDate:'date'},
+    {id: 8, textCategory:'front', textTitle:'title3', textBody:'bodybodybody3', createdDate:'date'},
+    {id: 9, textCategory:'front', textTitle:'title4', textBody:'bodybodybody4', createdDate:'date'},
+    {id: 10, textCategory:'server', textTitle:'title5', textBody:'bodybodybody5', createdDate:'date'},
+    {id: 11, textCategory:'server', textTitle:'title1', textBody:'bodybodybody1', createdDate:'date'},
+    {id: 12, textCategory:'server', textTitle:'title2', textBody:'bodybodybody2', createdDate:'date'},
+    {id: 13, textCategory:'server', textTitle:'title3', textBody:'bodybodybody3', createdDate:'date'},
+    {id: 14, textCategory:'server', textTitle:'title4', textBody:'bodybodybody4', createdDate:'date'},
+    {id: 15, textCategory:'server', textTitle:'title5', textBody:'bodybodybody5', createdDate:'date'},
 ]
 
 let id = 5;
@@ -50,4 +60,13 @@ app.get('/list/:id', (req, res)=>{
     console.log('detail get detected');
     console.log(result[req.params.id-1])
     res.send(result[req.params.id-1]); 
+})
+
+let url = require('url');
+
+app.get('/api/list', (req, res)=>{
+    let reqUrl = req.url;
+    let parsedUrl = url.parse(reqUrl, true);
+
+    console.log(parsedUrl);
 })
